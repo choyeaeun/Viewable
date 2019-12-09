407655b8aa34950b9c923dc4ee3183ae0e47cfe2
choyeaeun <silbia629@gmail.com>
2019-12-09 21:45:14 +0900

    daumMap setting

---
+++ Viewable/Bridging_Header.h
@@ -0,0 +1,13 @@
+//
+//  Bridging_Header.h
+//  Viewable
+//
+//  Created by 조예은 on 09/12/2019.
+//  Copyright © 2019 Yeaeun. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+#import <DaumMap/MTMapView.h>
+#endif /* Bridging_Header_h */
