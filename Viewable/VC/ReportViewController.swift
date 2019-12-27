91997f03ad87c1e8dd12d4d33ce4b7e259e43eef
KimDeokwon <codevillain@naver.com>
2019-12-27 23:45:46 +0900

    신고 뷰 추가
    Signed-off-by: KimDeokwon <codevillain@naver.com>

--- Viewable/VC/ReportViewController.swift
+++ Viewable/VC/ReportViewController.swift
@@ -7,3 +7,59 @@
 //
 
 import Foundation
+
+class ReportViewController: UIViewController {
+    
+    // MARK:- Property
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var titleTextField: UITextField!
+    @IBOutlet var contentImageView: UIImageView!
+    @IBOutlet var contentTextView: UIReportTextView!
+    @IBOutlet var confrimView: UIView!
+    
+    private let pickerController: UIImagePickerController = {
+        let result = UIImagePickerController()
+        result.allowsEditing = false
+        result.sourceType = .photoLibrary
+        result.mediaTypes = ["public.image"]
+        return result
+    }()
+    
+    // MARK:- Method
+    @IBAction func didClickedContentImage(_ sender: Any) {
+        present(pickerController, animated: true, completion: nil)
+    }
+
+    @IBAction func didClickedSubmit(_ sender: Any) {
+        confrimView.isHidden = false
+    }
+
+    @IBAction func didClickedRealSubmit(_ sender: Any) {
+        confrimView.isHidden = true
+    }
+
+    @IBAction func didClickedNoSubmit(_ sender: Any) {
+        confrimView.isHidden = true
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        pickerController.delegate = self
+    }
+}
+
+extension ReportViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+//            imageView.contentMode = .scaleAspectFit
+            contentImageView.image = pickedImage
+        }
+        
+        dismiss(animated: true, completion: nil)
+    }
+}
