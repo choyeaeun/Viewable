9c2c76a744cebb6d439e0cea2daf69485e3c4e96
choyeaeun <silbia629@gmail.com>
2019-12-27 23:50:58 +0900

    필터 뷰컨 추가

--- Viewable/VC/AR/ShopfilterVC.swift
+++ Viewable/VC/AR/ShopfilterVC.swift
@@ -2,14 +2,16 @@
 //  ShopfilterVC.swift
 //  Viewable
 //
-//  Created by 조예은 on 21/12/2019.
+//  Created by 조예은 on 22/12/2019.
 //  Copyright © 2019 Yeaeun. All rights reserved.
 //
 
 import UIKit
 
 class ShopfilterVC: UIViewController {
-
+    
+    @IBOutlet var collection:[UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -18,5 +20,19 @@ class ShopfilterVC: UIViewController {
     @IBAction func selectedBtn(_ sender: UIButton) {
         sender.isSelected = !sender.isSelected
     }
+    @IBAction func dismiss(_ sender: UIButton) {
+        self.dismiss(animated: true)
+    }
     
+    @IBAction func resetFilter(_ sender: UIButton) {
+        if let buttons = collection {
+            for i in 0 ... buttons.count-1{
+                collection[i].isSelected = false
+            }
+        }
+    }
+    @IBAction func adjustFilter(_ sender: UIButton) {
+        self.dismiss(animated: true)
+    }
 }
+
