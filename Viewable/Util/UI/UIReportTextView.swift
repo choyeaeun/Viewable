a0d92a2bf286be7f9800220795fb577a5bb0fdc9
KimDeokwon <codevillain@naver.com>
2019-12-27 23:45:33 +0900

    신고 뷰 용 텍스트 뷰 추가
    Signed-off-by: KimDeokwon <codevillain@naver.com>

--- Viewable/Util/UI/UIReportTextView.swift
+++ Viewable/Util/UI/UIReportTextView.swift
@@ -7,3 +7,51 @@
 //
 
 import Foundation
+
+@IBDesignable
+class UIReportTextView: UITextView {
+
+    @IBInspectable var placeholder: String = ""
+    @IBInspectable var placeholderColor: UIColor = UIColor.gray
+    @IBInspectable var normalColor: UIColor = UIColor.black
+    
+    override init(frame: CGRect, textContainer: NSTextContainer?) {
+        super.init(frame: frame, textContainer: textContainer)
+        delegate = self
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        delegate = self
+//        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setTextValue() {
+        if text == placeholder {
+            text = ""
+            textColor = normalColor
+        } else if text == "" {
+            text = placeholder
+            textColor = placeholderColor
+        }
+    }
+    
+    override func layoutSubviews() {
+        let color = CGFloat(155 / 255)
+        layer.borderWidth = 1.0
+        layer.borderColor = UIColor(red: color, green: color, blue: color, alpha: 1).cgColor
+    }
+}
+
+extension UIReportTextView: UITextViewDelegate {
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        setTextValue()
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if text == "" {
+            setTextValue()
+        }
+    }
+}
